607f38851dd257635060cc08b15d2945f509e29a
TM_Jesus <j.tapia05@ufromail.cl>
2024-09-08 21:00:55 -0300

    Corregir método esPrimo para verificar si un número es primo e implementar lógica en el método main para llamar al nuevo método

--- Integracion/src/main/java/Main.java
+++ Integracion/src/main/java/Main.java
@@ -1,6 +1,29 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
-        System.out.println("Integración Intellij IDEA y GitHub");
+        System.out.println("Manejando el repositorio desde GitHub");
+        System.out.println("Agregando un mensaje para ver el «Amend»");
+
+        // ----- esPrimo -----
+        int num             = 5;
+        String resultado    = esPrimo(num) 
+            ? "El número " + num + " es primo" 
+            : "El número " + num + " NO es primo"; 
+        System.out.println(resultado);
+    }
+
+    public static boolean esPrimo(int num) {
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
+
+
+
+
+
+
